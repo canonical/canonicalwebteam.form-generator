07f956cfa18abfd21e1d5a2533b2f4348ef76df2
Peter French <peterfrench94@gmail.com>
2025-03-18 15:23:21 +0100

    initial: Setup linter & update readme

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="canonicalwebteam.form-generator",
-    version="0.0.1",
+    version="0.0.2",
     author="Canonical webteam",
     author_email="webteam@canonical.com",
     url="https://github.com/canonical/canonicalwebteam.form-generator",
