097a8a86baa2b3439ceba89527b9b027719ec441
Benjamin Oni <onibenjo@gmail.com>
2026-01-09 14:49:15 +0100

    feat: update version to 2.2.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="canonicalwebteam.form-generator",
-    version="2.1.1",
+    version="2.2.0",
     author="Canonical webteam",
     author_email="webteam@canonical.com",
     url="https://github.com/canonical/canonicalwebteam.form-generator",
